611ca7a2b704e829673db54bf34d7d7f3102b97f
Joao Vitor <joaovitormoraveis@gmail.com>
2023-02-10 20:11:21 -0500

    first commit

---
+++ src/main/java/me/joao/springwiththymeleaf/SpringWithThymeleafApplication.java
@@ -0,0 +1,13 @@
+package me.joao.springwiththymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWithThymeleafApplication.class, args);
+	}
+
+}
