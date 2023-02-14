bca4334a94450fb7e95e3767b2aa315798276a87
Joao Vitor <joaovitormoraveis@gmail.com>
2023-02-13 21:46:57 -0500

    Adding base mvc layers

--- src/main/java/me/joao/springwiththymeleaf/controller/EmployeeController.java
+++ src/main/java/me/joao/springwiththymeleaf/controller/EmployeeController.java
@@ -1,32 +1,30 @@
 package me.joao.springwiththymeleaf.controller;
 
 import me.joao.springwiththymeleaf.entity.Employee;
+import me.joao.springwiththymeleaf.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.annotation.PostConstruct;
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 @RequestMapping("/employees")
 public class EmployeeController {
 
-    List<Employee> employees;
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
 
     @GetMapping("/list")
     public String getEmployee(Model model) {
+        List<Employee> employees = employeeService.findAll();
+
         model.addAttribute("employees", employees);
         return "list-employees";
     }
 
-    @PostConstruct
-    private void loadData() {
-        employees = new ArrayList<>();
-        employees.add(Employee.builder().id(1).firstName("Employee").lastName("First").email("employeefirst@test.com").build());
-        employees.add(Employee.builder().id(2).firstName("Employee").lastName("Second").email("employeesecond@test.com").build());
-        employees.add(Employee.builder().id(2).firstName("Employee").lastName("Third").email("employeethirdt@test.com").build());
-    }
 }
